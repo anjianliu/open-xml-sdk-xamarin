5074dc8465a9af51c75fac24dcac7c411be5e6c7
Chris Banks <chris.daniel.banks@gmail.com>
2015-01-23 04:54:20 -0800

    Add iOS version

---
+++ src/OpenXmlSdk.iOS/OpenXmlSdk.iOS.TestClient/OpenXmlSdk.iOS.TestClientViewController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace OpenXmlSdk.iOS.TestClient
+{
+	public partial class OpenXmlSdk_iOS_TestClientViewController : UIViewController
+	{
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public OpenXmlSdk_iOS_TestClientViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			var doc = new WordprocessingDocument ();
+			if (doc != null) {
+			}
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+	}
+}
+
